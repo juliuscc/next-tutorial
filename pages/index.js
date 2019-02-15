414e866a157e40f2688944025e86daddb2c332b3
jcelik <julius.cc@hotmail.com>
2019-02-15 20:08:33 +0100

    Added styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
-class Index extends Component {
+const Wrapper = styled.div`
+	border: 2px solid #aaa;
+	border-radius: 5px;
+	padding: 20px 50px;
+`
+
+const Title = styled.h1`
+	font-family: Helvetica, sans-serif;
+`
+
+class index extends Component {
 	render() {
-		return <div>Hello world!</div>
+		return (
+			<Wrapper>
+				<Title>Hello world!</Title>
+			</Wrapper>
+		)
 	}
 }
 
-export default Index
+export default index
